2e608a8c00a7e6a959f904c358cbc1b253a504ca
imamovicdze <imamovicdze@gmail.com>
2018-12-10 02:01:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# javamanagementsystem
+
+External libraries that I use for this app is: 
+
+* mysql-connector-java-8.0.13.jar
+* jTattoo-1.6.11.jar
+* rs2xml.jar
+
+This application is written in Java using JDK 8, NetBeans 8.2 also support and SQL database. You should import database manually.
---
+++ src/Connect.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Connect {
+    
+    Connection conn = null;
+    
+    public static Connection getCon() {
+        
+        try {
+            
+            Class.forName("java.sql.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/management", "root", "");
+            
+            return conn;
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            
+                JOptionPane.showMessageDialog(null, e);
+                return null;
+                
+        }
+    }
+}
